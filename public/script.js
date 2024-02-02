6f8d9d2cd0d49bbc795a963cc29646d461846757
abraham <abrahamlawson67@gmail.com>
2024-02-02 13:03:30 +0100

    Create classe for to retrieve the values of the form, Create Interfaces HasHtmlFormat for implement class Data and use the method htmlFormat for return the invoice in document in html format

--- public/script.js
+++ public/script.js
@@ -1,18 +1,2 @@
-"use strict";
-console.log('okgggg');
-//Recupereation 
-const form = document.getElementById('form');
-const type = document.getElementById('type');
-const firstName = document.getElementById('firstName');
-const lastName = document.getElementById('lastName');
-const address = document.getElementById('address');
-const country = document.getElementById('country');
-const town = document.getElementById('town');
-const zip = document.getElementById('zip');
-const product = document.getElementById('product');
-const price = document.getElementById('price');
-const quantity = document.getElementById('quantity');
-const tva = document.getElementById('tva');
-form.addEventListener('submit', (e) => {
-    console.log(type.value, firstName.value, lastName.value, address.value, country.value, town.value, zip.value, product.value, price.value, quantity.value, tva.value);
-});
+import { FormInput } from './Classes/FormInput.js';
+new FormInput();
